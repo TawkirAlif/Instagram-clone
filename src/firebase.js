41e3c9bb850fa6673640dac6e1d0f5cd359bb548
Tawkir Alif <tawkiralifa@gmail.com>
2020-12-31 18:08:48 +0600

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+        apiKey: "AIzaSyB04wg4TvKNCHproDO5bB5TMZBn6WuZGNU",
+        authDomain: "instagram-beta-com.firebaseapp.com",
+        projectId: "instagram-beta-com",
+        storageBucket: "instagram-beta-com.appspot.com",
+        messagingSenderId: "278387909663",
+        appId: "1:278387909663:web:018b904cb62fad42d4212a",
+        measurementId: "G-FV8PT0B3FE"
+      };
